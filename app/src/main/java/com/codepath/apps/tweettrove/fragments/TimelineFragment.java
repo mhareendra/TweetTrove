fa65e3937411d9eadbe59a0db8a5812d198ba43c
Hareendra Manuru <hareendra.manuru@gmail.com>
2016-08-14 21:51:07 -0700

    bug fix and gif

--- app/src/main/java/com/codepath/apps/tweettrove/fragments/TimelineFragment.java
+++ app/src/main/java/com/codepath/apps/tweettrove/fragments/TimelineFragment.java
@@ -301,6 +301,9 @@ public abstract class TimelineFragment extends Fragment
 
     public long getLowestId()
     {
+
+        if(tweets.size() < 1)
+            return -1;
         long currentMinId = tweets.get(tweets.size() - 1).getUid();
         return currentMinId;
     }
